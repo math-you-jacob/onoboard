208d16ccf57258ea1811e7534fc228d4e48d3a59
math-you-jacob <39838363+math-you-jacob@users.noreply.github.com>
2023-06-03 21:32:21 +0530

    Add files via upload

---
+++ onoboard.sql
@@ -0,0 +1,174 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 03, 2023 at 04:53 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `onoboard`
+--
+CREATE DATABASE IF NOT EXISTS `onoboard` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `onoboard`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `announcements`
+--
+
+CREATE TABLE `announcements` (
+  `POSTID` int(11) NOT NULL,
+  `PUBLISHERID` int(11) NOT NULL,
+  `MODE` varchar(30) NOT NULL,
+  `CONTENT` varchar(1000) NOT NULL,
+  `TIMESTAMP` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `TAG` varchar(1000) NOT NULL,
+  `INTERESTED` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `audience`
+--
+
+CREATE TABLE `audience` (
+  `USERID` int(11) NOT NULL,
+  `PUBLISHERID` int(11) NOT NULL,
+  `REQUEST_STATUS` varchar(15) DEFAULT '"pending"'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complaints`
+--
+
+CREATE TABLE `complaints` (
+  `COMPLAINTID` int(11) NOT NULL,
+  `USERID` int(11) NOT NULL,
+  `COMPLAINT` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `publisher_requests`
+--
+
+CREATE TABLE `publisher_requests` (
+  `USERID` int(11) NOT NULL,
+  `REQUEST` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `saved`
+--
+
+CREATE TABLE `saved` (
+  `USERID` int(11) NOT NULL,
+  `POSTID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `USERID` int(11) NOT NULL,
+  `EMAIL` varchar(100) NOT NULL,
+  `PASSWORD` varchar(50) NOT NULL,
+  `FIRSTNAME` varchar(50) NOT NULL,
+  `LASTNAME` varchar(50) NOT NULL,
+  `PHONE` varchar(15) NOT NULL,
+  `REGION` varchar(100) NOT NULL,
+  `TAGS` varchar(500) NOT NULL,
+  `USERMODE` varchar(10) NOT NULL DEFAULT 'normal'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `announcements`
+--
+ALTER TABLE `announcements`
+  ADD PRIMARY KEY (`POSTID`);
+
+--
+-- Indexes for table `audience`
+--
+ALTER TABLE `audience`
+  ADD PRIMARY KEY (`USERID`,`PUBLISHERID`);
+
+--
+-- Indexes for table `complaints`
+--
+ALTER TABLE `complaints`
+  ADD PRIMARY KEY (`COMPLAINTID`);
+
+--
+-- Indexes for table `publisher_requests`
+--
+ALTER TABLE `publisher_requests`
+  ADD PRIMARY KEY (`USERID`);
+
+--
+-- Indexes for table `saved`
+--
+ALTER TABLE `saved`
+  ADD PRIMARY KEY (`USERID`,`POSTID`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`USERID`),
+  ADD UNIQUE KEY `EMAIL` (`EMAIL`),
+  ADD UNIQUE KEY `PHONE` (`PHONE`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `announcements`
+--
+ALTER TABLE `announcements`
+  MODIFY `POSTID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `complaints`
+--
+ALTER TABLE `complaints`
+  MODIFY `COMPLAINTID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `USERID` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
